b8e75decbcac054e8c781ba23228491eb5d835ca
lakshaypahujaepam <lakshay_pahuja@epam.com>
2026-03-03 10:18:42 +0530

    SOLID - O principle added

---
+++ softwareEngineeringBestPractices/designPrinciples/solid/solid/src/main/java/com/lakshay/openclosedprinciple/openclosedprinciple/AreaCalculator.java
@@ -0,0 +1,44 @@
+package com.lakshay.openclosedprinciple.openclosedprinciple;
+
+// 2️⃣ Open/Closed Principle (OCP)
+
+//✅ Correct – Extend, don’t modify
+//✔ Add new shape → just create new class. No modification needed.
+
+interface Shape {
+    double calculateArea();
+}
+
+class Circle implements Shape {
+    double radius;
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle implements Shape {
+    double length, width;
+    public double calculateArea() {
+        return length * width;
+    }
+}
+
+class AreaCalculator {
+    double calculate(Shape shape) {
+        return shape.calculateArea();
+    }
+}
+
+// ❌ Wrong – Modify class when adding new shape
+//____________________________________________________
+//public class AreaCalculator {
+//    double calculate(Object shape) {
+//        if (shape instanceof Circle) {
+//            return Math.PI * ((Circle) shape).radius * ((Circle) shape).radius;
+//        } else if (shape instanceof Rectangle) {
+//            return ((Rectangle) shape).length * ((Rectangle) shape).width;
+//        }
+//        return 0;
+//    }
+//}
+//____________________________________________________
